d648af2f8cdfd1f7162be2d0d7ff04f9c25448e0
pranavman11 <42564227+pranavman11@users.noreply.github.com>
2024-09-11 23:47:54 +0000

    Added Test(2)

--- hw2_test.py
+++ hw2_test.py
@@ -5,3 +5,9 @@ def test_random_sorted_array():
     expected_sorted_array = [1,4,10,14,27,58]
     actual_sorted_array = merge_sort(array)
     assert actual_sorted_array == expected_sorted_array
+
+def test_sorted_asc_array():
+    array = [4,15,18,23,35,41]
+    expected_sorted_array = [4,15,18,23,35,41]
+    actual_sorted_array = merge_sort(array)
+    assert actual_sorted_array == expected_sorted_array
